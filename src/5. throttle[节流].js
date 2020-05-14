ba77b61afc1d7ace917e3dbef39ed5b68bcc59c1
Alfred-Lau <jian.liu@icloud.com>
2020-05-14 19:05:48 +0800

    feat: add throttle

--- src/5. throttle[节流].js
+++ src/5. throttle[节流].js
@@ -11,4 +11,38 @@
 而函数节流侧重于一段时间内只执行一次。
 
 */
-exports.throttle = (fn, wait) => {};
+
+/* throttle 英文是 油门 的意思；debounce 是 去抖动【防抖】的意思 */
+exports.throttle = (fn, wait) => {
+  let timer = null;
+  let previous = +new Date();
+
+  return function (...args) {
+    let now = +new Date();
+
+    if (now - previous < wait) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        // 切换时间为当前时间很关键，这是一个在整个运行时函数上下文都存在的时间，相当于是 React
+        // 的 useRef，相当于是内存数据库！！！
+        previous = now;
+        fn.apply(this, args);
+      }, wait);
+    } else {
+      // 切换时间为当前时间很关键，这是一个在整个运行时函数上下文都存在的时间，相当于是 React
+      // 的 useRef，相当于是内存数据库！！！
+      previous = now;
+      fn.apply(this, args);
+    }
+  };
+};
+
+/* test demo */
+
+const throttleFn = exports.throttle(() => {
+  console.log('我是节流，我被执行了');
+}, 5000);
+
+throttleFn();
