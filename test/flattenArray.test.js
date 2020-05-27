264a4c208e48048e18c46a45c776dafc849391f6
Alfred-Lau <jian.liu@icloud.com>
2020-05-27 11:21:36 +0800

    feat: add push pop

--- test/flattenArray.test.js
+++ test/flattenArray.test.js
@@ -30,9 +30,9 @@ describe('restFlatten test', function () {
   // test('should splitFlatten', function () {
   //   const arr = [1, [2, [3, [4, 5]]], 6];
   //   expect(splitFlatten(arr)).toStrictEqual([1, 2, 3, 4, 5, 6]);
+  // // });
+  // test('should parseFlatten', function () {
+  //   const arr = [1, [2, [3, [4, 5]]], 6];
+  //   expect(parseFlatten(arr)).toStrictEqual([1, 2, 3, 4, 5, 6]);
   // });
-  test('should parseFlatten', function () {
-    const arr = [1, [2, [3, [4, 5]]], 6];
-    expect(parseFlatten(arr)).toStrictEqual([1, 2, 3, 4, 5, 6]);
-  });
 });
